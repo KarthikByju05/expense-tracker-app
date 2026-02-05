6df04e9ac1fb319518df9cd9bd5c184b86e7bc0c
KarthikByju05 <karthik.byju334@gmail.com>
2026-02-05 23:56:43 +0530

    Sets UI for floating action button

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,8 +8,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // text controller
+  final newExpenseNameController = TextEditingController();
+  final newExpenseAmountController = TextEditingController();
+  // add a new expense
+  void addNewExpense() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Add a new expense"),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            //expense name
+            TextField(controller: newExpenseNameController),
+
+            // expense amount
+            TextField(controller: newExpenseAmountController),
+          ],
+        ),
+
+        actions: [
+          // save button
+          MaterialButton(onPressed: save, child: Text("SAVE")),
+
+          // cancel button
+          MaterialButton(onPressed: cancel, child: Text("CANCEL")),
+        ],
+      ),
+    );
+  }
+
+  void save() {}
+
+  void cancel() {}
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      floatingActionButton: FloatingActionButton(
+        onPressed: addNewExpense,
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
