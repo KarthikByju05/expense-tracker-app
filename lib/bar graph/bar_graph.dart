4cac4a4196937096feaed965019b26713ae8159a
KarthikByju05 <karthik.byju334@gmail.com>
2026-02-06 23:57:25 +0530

    add bar graph

---
+++ lib/bar graph/bar_graph.dart
@@ -0,0 +1,64 @@
+import 'package:expensetracker/bar%20graph/bar_data.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class MyBarGraph extends StatelessWidget {
+  final double? maxY;
+  final double sunAmount;
+  final double monAmount;
+  final double tueAmount;
+  final double wedAmount;
+  final double thurAmount;
+  final double friAmount;
+  final double satAmount;
+
+  const MyBarGraph({
+    super.key,
+    required this.maxY,
+    required this.sunAmount,
+    required this.monAmount,
+    required this.tueAmount,
+    required this.wedAmount,
+    required this.thurAmount,
+    required this.friAmount,
+    required this.satAmount,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // initialize the bar data
+    final barData = BarData(
+      sunAmount: sunAmount,
+      monAmount: monAmount,
+      tueAmount: tueAmount,
+      wedAmount: wedAmount,
+      thurAmount: thurAmount,
+      friAmount: friAmount,
+      satAmount: satAmount,
+    );
+
+    barData.initializeBarData();
+
+    return BarChart(
+      BarChartData(
+        maxY: maxY,
+        minY: 0,
+        barGroups: barData.barData
+            .map(
+              (data) => BarChartGroupData(
+                x: data.x,
+                barRods: [
+                  BarChartRodData(
+                    toY: data.y,
+                    color: Colors.grey[800],
+                    width: 20,
+                    borderRadius: BorderRadius.circular(0),
+                  ),
+                ],
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
